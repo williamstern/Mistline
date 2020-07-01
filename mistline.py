c26be8571d114d1c9b08f16c8c86998fbf1a1016
William <sternwill970@gmail.com>
2020-07-01 15:31:55 -0500

    created base file

---
+++ mistline.py
@@ -0,0 +1,18 @@
+import requests
+import geocoder
+
+#api_key = GA8VgLQpB0Whf3D7KDuHwbvii1LBmyie
+
+#url = "https://api.climacell.co/v3/weather/forecast/hourly"
+
+#response = requests.request("GET", url)
+
+#print(response)
+
+
+geo = geocoder.ip('me')
+lat = geo.latlng[0]
+lng = geo.latlng[1]
+
+print(lat)
+print(lng)
