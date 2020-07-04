3e85c1c0239f3884f44402a2d0f9d2eb723b19d4
William <sternwill970@gmail.com>
2020-07-04 14:43:24 -0500

    added fields

--- mistline.py
+++ mistline.py
@@ -11,7 +11,8 @@ def find_cordinates(user):
 
 
 (lt, lg) = find_cordinates("me")
-payload = {"lat": lt, 'lon': lg, "location_id": "me", "unit_system" : "us", "precipitation_probability": " "}
+fields = ["temp", "feels_like", "humidity", "wind_speed", "precipitation_probability"]
+payload = {"lat": lt, 'lon': lg, "location_id": "me", "unit_system" : "us", "precipitation_probability": fields, "apikey": "GA8VgLQpB0Whf3D7KDuHwbvii1LBmyie"}
 
 url = "https://api.climacell.co/v3/weather/forecast/hourly"
 response = requests.request("GET", url)
